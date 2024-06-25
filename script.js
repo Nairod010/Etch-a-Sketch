66bf3749bb8821e3bfffba858ee1e80d6c15a943
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-06-25 23:49:52 +0300

    added 16x16 grid and reset button(non functional yet)

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+const body = document.querySelector("body");
+const container = document.querySelector("div");
+const gridSquare = document.createElement("div");
+gridSquare.className = "grid";
+
+
+const resetButton = document.createElement("button");
+resetButton.className = "reset";
+resetButton.textContent = "RESET";
+resetButton.addEventListener("click", () => {
+
+    createGrid(16);
+});
+body.insertBefore(resetButton, container);
+
+let grid = document.createElement("div");
+grid.className = "square";
+grid.addEventListener("hover", () => {
+    grid.style.backgroundColor= "blue";
+});
+
+
+function createGrid(num){
+for(i=0;i<num;i++) {
+    for(j=0;j<num;j++) {
+        let grid = document.createElement("div");
+
+        grid.className = "square";
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor= "blue";
+        });
+       
+        container.appendChild(grid);
+    };
+};
+};
+
+
+createGrid(16);
+
+
+
