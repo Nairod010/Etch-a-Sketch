d577136f4e7a8bfc9265a4afde7897fab748a986
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-06-26 09:04:08 +0300

    added functionality to the reset button

--- script.js
+++ script.js
@@ -2,16 +2,9 @@ const body = document.querySelector("body");
 const container = document.querySelector("div");
 const gridSquare = document.createElement("div");
 gridSquare.className = "grid";
+container.appendChild(gridSquare);
 
 
-const resetButton = document.createElement("button");
-resetButton.className = "reset";
-resetButton.textContent = "RESET";
-resetButton.addEventListener("click", () => {
-
-    createGrid(16);
-});
-body.insertBefore(resetButton, container);
 
 let grid = document.createElement("div");
 grid.className = "square";
@@ -21,22 +14,44 @@ grid.addEventListener("hover", () => {
 
 
 function createGrid(num){
-for(i=0;i<num;i++) {
-    for(j=0;j<num;j++) {
-        let grid = document.createElement("div");
+    let dimension = 640/num
+    for(i=0;i<num;i++) {
+        for(j=0;j<num;j++) {
+            let square = document.createElement("div");
+            
+            square.className = "square";
+            square.style.height = dimension+"px";
+            square.style.width = dimension+"px";
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor= "blue";
+            });
 
-        grid.className = "square";
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor= "blue";
-        });
-       
-        container.appendChild(grid);
+            gridSquare.appendChild(square);
+        };
     };
-};
 };
 
 
 createGrid(16);
 
+function deleteSquares() {
+    let element = document.querySelector("div.grid");
+    
+    let child = element.lastElementChild;
+    while (child) {
+        element.removeChild(child);
+        child = element.lastElementChild;
+};
+};
+
+
+const resetButton = document.createElement("button");
+resetButton.className = "reset";
+resetButton.textContent = "RESET";
+resetButton.addEventListener("click", () => {
+    deleteSquares();
+    createGrid(100);
+});
+container.insertBefore(resetButton, gridSquare);
 
 
